8ee8667b39775cacde40c782c21edd8fb23921f0
Hassan Khan <hassankpisces@gmail.com>
2024-05-16 04:02:17 +0500

    Complete solo task react freecodecamp

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,21 +3,24 @@ import locationicon from "../images/location-icon.png"
 
 function Card(props) {
     return (
-        <div className="card">
-            <img className="card-image" src={props.item.imageUrl} alt="" />
-            <div className="card-info black" >
-                <div className="card-location">
-                    <span className="card-location-name">
-                        <img src={locationicon} alt="" />
-                        {props.item.location}
-                    </span>
-                    <a className="card-location-map" href="">View on Google Maps</a>
+        <>
+            <div className="card">
+                <img className="card-image" src={props.item.imageUrl} alt="" />
+                <div className="card-info black" >
+                    <div className="card-location">
+                        <span className="card-location-name">
+                            <img src={locationicon} alt="" />
+                            {props.item.location}
+                        </span>
+                        <a className="card-location-map" href={props.item.googleMapUrl} target="_blank">View on Google Maps</a>
+                    </div>
+                    <h2 className="card-title">{props.item.title}</h2>
+                    <p className="card-dates">{props.item.startDate} - {props.item.endDate}</p>
+                    <p className="card-description">{props.item.description}</p>
                 </div>
-                <h2 className="card-title">{props.item.title}</h2>
-                <p className="card-dates">{props.item.startDate} - {props.item.endDate}</p>
-                <p className="card-description">{props.item.description}</p>
             </div>
-        </div>
+            <hr />
+        </>
     )
 }
 export default Card
